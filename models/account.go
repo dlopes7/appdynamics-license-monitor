2fed2b22cc5e5b749ee014548d9d08e839a24912
David Ribeiro Lopes <dlopes@dlopes-mac.local>
2017-10-10 17:42:37 -0300

    More models required for the 3 extra metrics

---
+++ models/account.go
@@ -0,0 +1,6 @@
+package models
+
+// Account : struct used to get the Account details
+type Account struct {
+	ID string `json:"id"`
+}
--- models/license.go
+++ models/license.go
@@ -1,8 +1,8 @@
 package models
 
 type LicenseModules struct {
-	LicenseModule []LicenseModule `json:"modules"`
-	Links         []Link          `json:"links"`
+	LicenseModules []LicenseModule `json:"modules"`
+	Links          []Link          `json:"links"`
 }
 
 type Link struct {
@@ -13,4 +13,32 @@ type Link struct {
 type LicenseModule struct {
 	Name  string `json:"name"`
 	Links []Link `json:"links"`
+}
+
+// Properties : this is a list of Property
+type Properties struct {
+	Properties []Property `json:"properties"`
+}
+
+// Property : this is a Property of a license
+type Property struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// Usages : list of License Usage objects
+type Usages struct {
+	Usages []Usage
+}
+
+// Usage represents a single license usage for a period of 5 minutes
+type Usage struct {
+	ID               string `json:"id"`
+	AccountID        int    `json:"accountId"`
+	AgentType        string `json:"agentType"`
+	CreatedOn        int64  `json:"createdOn"`
+	CreatedOnISODate string `json:"createdOnIsoDate"`
+	UnitsUsed        int    `json:"unitsUsed"`
+	UnitsProvisioned int    `json:"unitsProvisioned"`
+	UnitsAllowed     int    `json:"unitsAllowed"`
 }
